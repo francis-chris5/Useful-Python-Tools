646cd3b5a3baf67b995a1b5410e3d9f3887190d5
francis-chris5 <50467171+francis-chris5@users.noreply.github.com>
2020-12-03 14:04:17 -0500

    Add files via upload

---
+++ test_create_class.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  3 13:45:27 2020
+
+@author: Christopher S. Francis
+"""
+
+import ClassGenerator
+	# put functions in a module without class structures
+#ClassGenerator.startFunction()
+
+#ClassGenerator.startFunction(parameters=["i", "j", "k"])
+
+
+ ####################################################
+ ##	                                               ##
+ ##   DELETE THE TEST 'myModule.py' AT THIS POINT  ##
+ ##   THEN COMMENT OUT LINES ABOVE HERE            ##
+ ##   AND UNCOMMENT LINES BELOW HERE               ##
+ ##	                                               ##
+ ####################################################
+
+
+
+
+ClassGenerator.startClass(attributes=["x", "y", "z"])
+
+ClassGenerator.startFunction()
+
+ClassGenerator.startFunction(parameters=["i", "j", "k"])
